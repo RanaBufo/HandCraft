fcfe3d72e514bf231ddcd7639cc2c1fc76f40646
RanaBufo <milly.ili000@mail.ru>
2024-05-03 01:54:23 +0300

    DataBase

---
+++ Backend/DataBase/ContactsDB.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandCraft.DataBase
+{
+    public class ContactsDB
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public int IdUser { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Phone { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
---
+++ Backend/DataBase/UserRoleDB.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HandCraft.DataBase
+{
+    public class UserRoleDB
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public int IdUser { get; set; }
+        [Required]
+        public int IdRole { get; set; }
+    }
+}
