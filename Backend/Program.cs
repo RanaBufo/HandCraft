c23d938cd376c131d95bc03cad2850c3b1b27de7
RanaBufo <milly.ili000@mail.ru>
2024-05-04 20:08:10 +0300

    RoleDB

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,18 +1,38 @@
 ﻿using HandCrafter;
+using HandCrafter.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Services
+builder.Services.AddSingleton<ValidService>();
+builder.Services.AddScoped<RoleService>();
+
 //BD
 builder.Services.AddDbContext<ApplicationContext>(options =>
 {
-    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    string connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "default connection string";
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
 });
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
+app.UseCors("AllowOrigin");
+app.UseHttpsRedirection();
+
+app.MapControllers();
 app.Run();
